b14e48116af1dd2b9567965a407f294b196c16c7
sefu <zephaniahsefuoduori@gmail.com>
2022-12-14 19:32:22 +0300

    Initial commit

---
+++ src/main/kotlin/com/bank/thebankcrud/ThebankcrudApplication.kt
@@ -0,0 +1,11 @@
+package com.bank.thebankcrud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThebankcrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThebankcrudApplication>(*args)
+}
---
+++ src/test/kotlin/com/bank/thebankcrud/ThebankcrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bank.thebankcrud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThebankcrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
